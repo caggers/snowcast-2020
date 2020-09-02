300db5cd2394282fca65782ee8ee66c1511a8ceb
caggers <gemmacagney@gmail.com>
2020-09-01 17:37:17 -0700

    remove async code from select test

--- src/components/Select/Select.spec.tsx
+++ src/components/Select/Select.spec.tsx
@@ -8,13 +8,13 @@ describe("Select", () => {
 		resortid: 222036
 	}
 
-	it("renders the App component correctly", async () => {
-		const { findByText } = render(<Select location={location} />)
-		await findByText("Ellmau")
+	it("renders the Select component correctly", () => {
+		const { getByText } = render(<Select location={location} />)
+		getByText("Ellmau")
 	})
 
 
-	it("can select a new value", async () => {
+	it("can select a new value", () => {
 		const { getByTestId } = render(<Select location={location} />)
 		fireEvent.change(getByTestId("select"), { target: { value: "Westendorf" }, })
 	})
