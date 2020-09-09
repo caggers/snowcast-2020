f37bd4e7d4cb923847c833b1f609e09ae430e837
caggers <gemmacagney@gmail.com>
2020-09-09 14:27:55 -0700

    fix weird background

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,21 +2,17 @@ import * as React from 'react'
 import { useState, useEffect, FunctionComponent } from 'react'
 import { getData, buildTextArray, getForecast } from '../../util/util'
 import { AxiosResponse } from 'axios'
-import styled, { ThemeProvider } from 'styled-components'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import { ISnowReportData, ITodaysForecast, text, option } from '../../types/api'
 import { theme } from '../../util/themes'
 import Card from '../Card/Card'
 
-const Background = styled.div`
-	background-color: rgba(238, 238, 238, 0.5);
-	color: white;
-	font-family: 'Public Sans', sans-serif;
-	height: 2000px;
-	left: 0;
-	min-width: 1024px;
-	position: fixed;
-	top: 0;
-	width: 100%;
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: rgba(238, 238, 238, 0.5);
+		color: white;
+		font-family: 'Public Sans', sans-serif;
+  }
 `
 
 const App: FunctionComponent = () => {
@@ -70,18 +66,12 @@ const App: FunctionComponent = () => {
 		<>
 			{snowReport !== null && panelText !== null && (
 				<ThemeProvider theme={theme}>
-					<Background>
-						<Card
-							handleClickOption={handleClickOption}
-							panelText={panelText}
-							snowReport={snowReport}
-						/>
-						<Card
-							handleClickOption={handleClickOption}
-							panelText={panelText}
-							snowReport={snowReport}
-						/>
-					</Background>
+					<GlobalStyle />
+					<Card
+						handleClickOption={handleClickOption}
+						panelText={panelText}
+						snowReport={snowReport}
+					/>
 				</ThemeProvider>
 			)}
 		</>
